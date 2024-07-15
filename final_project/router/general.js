1a8675cf0bcaba65ba43dd4e3a52c06fe1f59f94
Your Name <you@example.com>
2024-07-15 09:26:36 -0400

    Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,28 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = parseInt(req.params.isbn);
+  const seletedBook = books[isbn];
+  res.send(seletedBook);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const selectedBooks = {};
+  for (let i in books) {
+    if (books[i].author == author) {
+        selectedBooks[i] = books[i];
+    }
+  }
+  res.send(selectedBooks);
 });
 
 // Get all books based on title
